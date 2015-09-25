a8cf036772d65d24c8128e817705e8e7cc1ff5f7
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2015-09-25 11:14:08 -0400

    Update dontquit.js

--- dontquit.js
+++ dontquit.js
@@ -1,6 +1,6 @@
 var dontquit = {
     defaultText: "Are you sure you want to leave?",
-    Version: 1.0.1,
+    Version: 1.01,
     Author: "Cosmic Web Services",
 	do: function(e){
         if(!e) e = window.event;
